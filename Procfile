1780f3ec63c0d885ee3867490b4e64117c0fd669
Prakash Sejwani <prakashsejwani@gmail.com>
2024-11-28 18:16:00 +0530

    Add active storage for adding image to posts

--- Procfile
+++ Procfile
@@ -1,2 +1,2 @@
-web: bundle exec rails server -b 0.0.0.0
+web: ./bin/thrust bundle exec rails server -b 0.0.0.0
 worker: bundle exec good_job start
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,5 @@
 class Post < ApplicationRecord
+  has_one_attached :image do |attachable|
+    attachable.variant :thumb_medium, resize_to_fill: [ 900, 350, crop: :attention ]
+  end
 end
