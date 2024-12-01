4c8949602d8c20ecd1b523d672602a0be5b8703b
Prakash Sejwani <prakashsejwani@gmail.com>
2024-12-01 10:40:49 +0530

    Remove config host do server ip in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -81,7 +81,7 @@ Rails.application.configure do
   config.active_record.attributes_for_inspect = [ :id ]
 
   # Enable DNS rebinding protection and other `Host` header attacks.
-  config.hosts << "139.59.64.20"
+  # config.hosts << "139.59.64.20"
   # config.hosts = [
   #   "example.com",     # Allow requests from example.com
   #   /.*\.example\.com/ # Allow requests from subdomains like `www.example.com`
